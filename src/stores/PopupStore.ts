9b9804c3793c11895b701d3a39682d1855dbc763
Татьяна Хонякина <hanakinat@gmail.com>
2023-11-02 15:19:08 +0300

    Добавила тип для функции

--- src/stores/PopupStore.ts
+++ src/stores/PopupStore.ts
@@ -1,11 +1,13 @@
 import {defineStore} from "pinia";
 import {ref} from "vue";
 
+type callback = () => {};
+
 export const usePopupStore = defineStore('popup', () => {
     const onOK = ref(() => {});
     const show = ref(false);
 
-    function showPopup(callback) {
+    function showPopup(callback: callback) {
         onOK.value = callback;
         show.value = true;
     }
